3b056dcc7ced93f046d403f5ad4fb48b52470b4f
Corey Light <coreylight@gmail.com>
2018-02-22 13:54:40 -0600

    Bump number of plugins expected in test

--- index.test.js
+++ index.test.js
@@ -6,14 +6,14 @@ describe('iopipe kitchen sink', () => {
     delete process.env.IOPIPE_TOKEN;
   });
 
-  it('has trace plugin pre-bundled', done => {
+  it('has trace and event info plugins pre-bundled', done => {
     iopipe({ clientId: 'foobar' })((event, context) => {
       try {
         const { config } = context.iopipe;
 
         expect(config.extends).toBe('@iopipe/config');
 
-        expect(config.plugins.length).toBe(1);
+        expect(config.plugins.length).toBe(2);
 
         expect(_.isFunction(config.plugins[0])).toBe(true);
 
