dc6033d10271eff98e9fe5177e2e15187c86ccda
Corey Light <coreylight@gmail.com>
2018-05-03 11:14:44 -0500

    feat(context functions): expose core functions for ease of use (#14)
    feat(context functions): expose core functions for ease of use
    By exposing functions like mark, metric, and label at the module level,
    consumers of this library will have an easier time using those features.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 module.exports = require('@iopipe/core');
+
+function warning(fn, ...args) {
+  /*eslint-disable no-console*/
+  const str = `${fn}(${args.join(
+    ','
+  )}) was called from @iopipe/iopipe but this method was not available. You may have called this method outside of an invocation or @iopipe/core needs to be upgraded to satisfy version ^1.11.0`;
+  console.warn(str);
+  return str;
+  /*eslint-enable no-console*/
+}
+
+function ctx() {
+  const context =
+    (typeof module.exports.getContext === 'function' &&
+      module.exports.getContext()) ||
+    {};
+  return (
+    context.iopipe || {
+      mark: {
+        start: warning.bind(null, 'mark.start'),
+        end: warning.bind(null, 'mark.end')
+      },
+      metric: warning.bind(null, 'metric'),
+      label: warning.bind(null, 'label')
+    }
+  );
+}
+
+module.exports.mark = {
+  start: (...args) => ctx().mark.start(...args),
+  end: (...args) => ctx().mark.end(...args)
+};
+
+module.exports.metric = (...args) => ctx().metric(...args);
+module.exports.label = (...args) => ctx().label(...args);
--- src/index.test.js
+++ src/index.test.js
@@ -1,19 +1,39 @@
-const _ = require('lodash');
-const iopipe = require('./index');
+import _ from 'lodash';
+import mockContext from 'aws-lambda-mock-context';
+
+import iopipe from '.';
+
+beforeEach(() => {
+  delete process.env.IOPIPE_TOKEN;
+});
+
+test('lib warns correctly for getContext methods', () => {
+  const { mark: { start, end }, metric, label } = iopipe;
+  const messages = [
+    start('test-mark'),
+    end('try-mark'),
+    metric('test-metric-key', 'test-metric-value'),
+    label('great-label')
+  ];
+  expect(messages).toMatchSnapshot();
+});
 
 describe('iopipe kitchen sink', () => {
-  beforeEach(() => {
-    delete process.env.IOPIPE_TOKEN;
-  });
-
   it('has trace and event info plugins pre-bundled', done => {
-    iopipe({ clientId: 'foobar' })((event, context) => {
+    let invocation;
+    iopipe({
+      clientId: 'foobar',
+      plugins: [inv => (invocation = inv) && { meta: { name: 'wow' } }]
+    })((event, context) => {
       try {
         const { config } = context.iopipe;
 
-        expect(config.extends).toBe('@iopipe/config');
-
-        expect(config.plugins).toHaveLength(2);
+        expect(invocation.plugins).toHaveLength(3);
+        expect(invocation.plugins.map(p => p.meta.name)).toEqual([
+          'wow',
+          '@iopipe/trace',
+          '@iopipe/event-info'
+        ]);
 
         expect(_.isFunction(config.plugins[0])).toBe(true);
 
@@ -25,4 +45,39 @@ describe('iopipe kitchen sink', () => {
       }
     })({}, {});
   });
+});
+
+test('lib has mark, metric, label methods', async () => {
+  const ctx = mockContext({ functionName: 'mark' });
+  let invocation;
+
+  iopipe({ token: 'trace', plugins: [inv => (invocation = inv)] })((e, c) => {
+    const { mark: { start, end }, metric, label } = iopipe;
+    start('trace-test');
+    metric('foobar', 100);
+    label('animals');
+    label('cats');
+    setTimeout(() => {
+      end('trace-test');
+      c.succeed(200);
+    }, 101);
+  })({}, ctx);
+
+  const val = await ctx.Promise;
+  expect(val).toBe(200);
+
+  const {
+    performanceEntries,
+    labels,
+    custom_metrics: metrics
+  } = invocation.report.report;
+
+  expect(_.map(performanceEntries, 'name')).toEqual([
+    'start:trace-test',
+    'measure:trace-test',
+    'end:trace-test'
+  ]);
+  expect(performanceEntries[1].duration).toBeGreaterThan(101);
+  expect(metrics).toEqual([{ name: 'foobar', n: 100, s: undefined }]);
+  expect(labels).toEqual(['animals', 'cats']);
 });
