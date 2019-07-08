406a3e52f7257c3d935cd777bebb8a7d6089841b
Maurice Rickard <maurice@mauricerickard.com>
2019-07-08 11:35:17 -0400

    Removing Node 6 runtime support.

--- publish-layers.sh
+++ publish-layers.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 
-NODEJS610_DIST=dist/nodejs610.zip
 NODEJS810_DIST=dist/nodejs810.zip
 NODEJS10_DIST=dist/nodejs10.zip
 
@@ -26,50 +25,11 @@ rm -rf dist
 mkdir -p dist
 
 function usage {
-    echo "./publish-layers.sh [nodejs6.10|nodejs8.10|nodejs10.x]"
+    echo "./publish-layers.sh [nodejs8.10|nodejs10.x]"
     exit 1
 }
 
 case "$1" in
-    "nodejs6.10")
-        echo "Building iopipe layer for nodejs6.10"
-        npm install --prefix nodejs @iopipe/iopipe
-        zip -rq $NODEJS610_DIST nodejs
-        rm -rf nodejs package-lock.json
-        echo "Build complete: ${NODEJS610_DIST}"
-
-        nodejs610_hash=$(git rev-parse HEAD)
-        nodejs610_s3key="iopipe-nodejs6.10/${nodejs610_hash}.zip"
-
-        for region in "${REGIONS[@]}"; do
-            bucket_name="iopipe-layers-${region}"
-
-            echo "Uploading ${NODEJS610_DIST} to s3://${bucket_name}/${nodejs610_s3key}"
-            aws --region $region s3 cp $NODEJS610_DIST "s3://${bucket_name}/${nodejs610_s3key}"
-
-            echo "Publishing nodejs6.10 layer to ${region}"
-            nodejs610_version=$(aws lambda publish-layer-version \
-                --layer-name IOpipeNodeJS610 \
-                --content "S3Bucket=${bucket_name},S3Key=${nodejs610_s3key}" \
-                --description "IOpipe Layer for Node 6.10" \
-                --compatible-runtimes nodejs6.10 \
-                --license-info "Apache 2.0" \
-                --region $region \
-                --output text \
-                --query Version)
-            echo "Published nodejs6.10 layer version ${nodejs610_version} to ${region}"
-
-            echo "Setting public permissions for nodejs6.10 layer version ${nodejs610_version} in ${region}"
-            aws lambda add-layer-version-permission \
-              --layer-name IOpipeNodeJS610 \
-              --version-number $nodejs610_version \
-              --statement-id public \
-              --action lambda:GetLayerVersion \
-              --principal "*" \
-              --region $region
-            echo "Public permissions set for nodejs6.10 layer version ${nodejs610_version} in region ${region}"
-        done
-        ;;
     "nodejs8.10")
         echo "Building iopipe Layer for nodejs8.10"
         npm install --prefix nodejs @iopipe/iopipe
